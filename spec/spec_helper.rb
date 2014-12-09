10f544fe837f5a148090a9bf0fa24f7260b6fff2
Charlie Sharpsteen <source@sharpsteen.net>
2014-12-09 08:35:15 -0800

    Add support for using future parser in spec tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,4 +3,5 @@ require 'rspec-puppet'
 RSpec.configure do |c|
   c.module_path = File.join(File.dirname(File.expand_path(__FILE__)), 'fixtures', 'modules')
   c.manifest_dir = File.join(File.dirname(File.expand_path(__FILE__)), 'fixtures', 'manifests')
+  c.parser = 'future' if ENV['FUTURE_PARSER'] == 'yes'
 end
